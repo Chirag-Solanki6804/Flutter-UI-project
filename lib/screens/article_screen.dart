848ffd8bc6061745d8f45661135492150e11128a
Chirag Solanki <104585228+Chirag-Solanki6804@users.noreply.github.com>
2023-02-14 23:59:04 +0530

    Add files via upload

---
+++ lib/screens/article_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import '../models/article_model.dart';
+import '../widgets/custom_tag.dart';
+import '../widgets/image_container.dart';
+
+class ArticleScreen extends StatelessWidget {
+  const ArticleScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/article';
+  @override
+  Widget build(BuildContext context) {
+    final article = ModalRoute.of(context)!.settings.arguments as Article;
+    return ImageContainer(
+      width: double.infinity,
+      imageUrl: article.imageUrl,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(color: Colors.white),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          children: [
+            _NewsHeadline(article: article),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _NewsHeadline extends StatelessWidget {
+  const _NewsHeadline({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+          ),
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
+            children: [
+              Text(
+                article.category,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Text(
+            article.title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  height: 1.25,
+                ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            article.subtitle,
+            style: Theme.of(context)
+                .textTheme
+                .bodyMedium!
+                .copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
